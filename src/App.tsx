3476c89d3acdd9943221a472235b675e93cef37d
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-01 23:42:08 +0800

    add sign up and log in page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Workout from "./pages/Workout";
 import Library from "./pages/Library";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 const App = () => {
   return (
     <Routes>
@@ -12,6 +14,8 @@ const App = () => {
         <Route path="workout-plans" element={<Workout />} />
         <Route path="exercise-library" element={<Library />} />
         <Route path="about-us" element={<About />} />
+        <Route path="sign-up" element={<Signup />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
