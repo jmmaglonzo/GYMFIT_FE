74b00ded054e0d07296421d8838c9e99d22d3729
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-02 17:40:12 +0800

    complete form function

---
+++ src/components/Workout/WorkoutDetails.tsx
@@ -0,0 +1,27 @@
+import { BiTrash } from "react-icons/bi";
+import { Workout } from "../../pages/Workout";
+
+interface WorkoutProps {
+  data: Workout;
+  deleteWorkout: (id: string) => Promise<void>;
+}
+
+const WorkoutDetails = ({ data, deleteWorkout }: WorkoutProps) => {
+  return (
+    <div className="bg-white flex  justify-between p-4 rounded-md  ">
+      <div className="flex flex-col gap-2">
+        <span className="font-bold text-2xl text-[#ef5350]">{data.title}</span>
+        <span className="font-bold text-xl">Load (KG): {data.load} </span>
+        <span className="font-bold text-xl">Reps: {data.reps}</span>
+      </div>
+
+      <BiTrash
+        className="cursor-pointer"
+        size={20}
+        onClick={() => deleteWorkout(data._id)}
+      />
+    </div>
+  );
+};
+
+export default WorkoutDetails;
--- src/pages/Workout.tsx
+++ src/pages/Workout.tsx
@@ -1,5 +1,174 @@
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import WorkoutDetails from "../components/Workout/WorkoutDetails";
+import axios from "axios";
+
+const url = "https://workout-crud.vercel.app/api/workouts";
+
+export interface Workout {
+  _id: string;
+  title: string;
+  reps: number;
+  load: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
 const Workout = () => {
-  return <div>Workout</div>;
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [form, setForm] = useState({
+    title: "",
+    load: "",
+    reps: "",
+  });
+  const [errors, setErrors] = useState({
+    title: "",
+    load: "",
+    reps: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    getWorkout();
+  }, []);
+
+  const getWorkout = async () => {
+    const res = await axios.get(url);
+    setWorkouts(res.data);
+  };
+
+  const createWorkout = async (e: FormEvent) => {
+    e.preventDefault();
+
+    // Form validation
+    let formValid = true;
+    const newErrors = {
+      title: "",
+      load: "",
+      reps: "",
+    };
+
+    if (!form.title.trim()) {
+      newErrors.title = "Title is required";
+      formValid = false;
+    } else if (/^\d+$/.test(form.title.trim())) {
+      newErrors.title = "Title should not contain numbers";
+      formValid = false;
+    }
+
+    if (!form.load.trim()) {
+      newErrors.load = "Load is required";
+      formValid = false;
+    } else if (isNaN(Number(form.load))) {
+      newErrors.load = "Load should be a number";
+      formValid = false;
+    }
+
+    if (!form.reps.trim()) {
+      newErrors.reps = "Reps are required";
+      formValid = false;
+    } else if (isNaN(Number(form.reps))) {
+      newErrors.reps = "Reps should be a number";
+      formValid = false;
+    }
+
+    if (!formValid) {
+      setErrors(newErrors);
+      return;
+    }
+
+    const res = await axios.post(url, form);
+
+    setWorkouts([...workouts, res.data]);
+    setForm({ title: "", load: "", reps: "" });
+    setErrors({ title: "", load: "", reps: "" });
+  };
+
+  const deleteWorkout = async (id: string) => {
+    await axios.delete(`https://workout-crud.vercel.app/api/workouts/${id}`);
+
+    const newWorkout = [...workouts].filter((w) => w._id !== id);
+
+    setWorkouts(newWorkout);
+  };
+
+  return (
+    <section className="md:py-20 my-12 container grid grid-cols-3 gap-12">
+      <div className="col-span-2 overflow-auto h-96 flex flex-col gap-4 p-4">
+        {workouts.length === 0 ? (
+          <span className="font-bold">Enter Data to start</span>
+        ) : (
+          <>
+            {workouts
+              .slice()
+              .sort(
+                (a, b) =>
+                  new Date(b.updatedAt || b.createdAt).getTime() -
+                  new Date(a.updatedAt || a.createdAt).getTime()
+              )
+              .map((el) => (
+                <WorkoutDetails
+                  key={el._id}
+                  data={el}
+                  deleteWorkout={deleteWorkout}
+                />
+              ))}
+          </>
+        )}
+      </div>
+      <div className="col-span-1">
+        <form
+          onSubmit={createWorkout}
+          className="flex flex-col bg-white flex-1 border border-black p-4 rounded-lg shadow-lg"
+        >
+          <h3 className="font-bold text-2xl">Add a new Workout</h3>
+
+          <label htmlFor="title">Exercise Title:</label>
+          <input
+            className="bg-slate-700 text-white px-4 py-2"
+            id="title"
+            type="text"
+            name="title"
+            value={form.title}
+            onChange={handleChange}
+          />
+          {errors.title && <span className="text-red-500">{errors.title}</span>}
+
+          <label htmlFor="load">Load in (in kg):</label>
+          <input
+            id="load"
+            className="bg-slate-700 text-white px-4 py-2"
+            type="number"
+            name="load"
+            value={form.load}
+            onChange={handleChange}
+          />
+          {errors.load && <span className="text-red-500">{errors.load}</span>}
+
+          <label htmlFor="reps">Reps:</label>
+          <input
+            id="reps"
+            className="bg-slate-700 text-white px-4 py-2"
+            type="number"
+            name="reps"
+            value={form.reps}
+            onChange={handleChange}
+          />
+          {errors.reps && <span className="text-red-500">{errors.reps}</span>}
+
+          <button type="submit" className="mt-8 px-6 py-2 bg-black text-white ">
+            Add Workout
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Workout;
