7251bc8764473ff3cb96cf24d3df27745c0f09cc
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-02 23:38:21 +0800

    add pagination

---
+++ src/components/Exercise/Pagination.tsx
@@ -0,0 +1,38 @@
+interface paginationTypes {
+  totalPosts: number;
+  postsPerPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  currentPage: number;
+}
+
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  setCurrentPage,
+  currentPage,
+}: paginationTypes) => {
+  const pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mt-12">
+      {pages.map((page, index) => {
+        return (
+          <button
+            className={`font-bold  px-4 py-1 ${
+              page === currentPage && "bg-black text-white"
+            } `}
+            key={index}
+            onClick={() => setCurrentPage(page)}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../components/Exercise/Card";
 import Loader from "../components/Loader/Loader";
+import Pagination from "../components/Exercise/Pagination";
 
 export interface ExerciseType {
   bodyPart: string;
@@ -17,6 +18,8 @@ export interface ExerciseType {
 const Library = () => {
   const [exercise, setExercise] = useState<ExerciseType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [postPerPage] = useState<number>(10);
   useEffect(() => {
     getExercise();
   }, []);
@@ -25,7 +28,7 @@ const Library = () => {
     setIsLoading(true);
     const res = await axios.get("https://exercisedb.p.rapidapi.com/exercises", {
       url: "https://exercisedb.p.rapidapi.com/exercises",
-      params: { limit: "50" },
+      params: { limit: "100" },
       headers: {
         "X-RapidAPI-Key": "41d5aec5e0mshf5632abd58571cbp1b37e7jsn36c8acffc7e4",
         "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
@@ -36,24 +39,34 @@ const Library = () => {
     setIsLoading(false);
   };
 
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+  const currentPosts = exercise.slice(firstPostIndex, lastPostIndex);
+
   return (
-    <section className="md:py-20 flex flex-col  my-12 container">
+    <section className="md:py-20 flex flex-col my-12 container">
       <div className="flex justify-center">
         <span className="font-bold text-4xl">
           Workouts & Routines: Elevate Your Fitness
         </span>
       </div>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-4 grid-cols-2">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-4 grid-cols-2 mt-12">
         {isLoading ? (
           <Loader />
         ) : (
           <>
-            {exercise.map((e) => (
+            {currentPosts.map((e) => (
               <Card key={e.id} exercise={e} />
             ))}
           </>
         )}
       </div>
+      <Pagination
+        totalPosts={exercise.length}
+        postsPerPage={postPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </section>
   );
 };
