5ea1e1146a06ab96bd91f9d77dea673cf35adf05
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-04 05:16:37 +0800

    fix mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery } from "@uidotdev/usehooks";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BiMenuAltRight, BiUser } from "react-icons/bi";
 import { NavLink } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
@@ -8,17 +8,38 @@ import { useAuthContext } from "../hooks/useAuthContext";
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const menuRef = useRef<HTMLElement>(null);
 
   const isSmallDevice = useMediaQuery("only screen and (max-width : 767px)");
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setToggle(true);
+      }
+    };
+    document.addEventListener("click", handler);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
   const handleClick = () => {
     logout();
   };
+
+  const closeMenu = () => {
+    setToggle(true);
+  };
+
   return (
-    <header className="fixed top-0 left-0 right-0 bg-custom-white w-full py-2  ">
+    <header className="fixed top-0 left-0 right-0 bg-custom-white w-full py-2">
       {isSmallDevice ? (
-        <nav className="flex justify-between px-4 container relative">
-          <NavLink to="/" className="font-bold">
+        // MOBILE NAV
+        <nav
+          className="flex justify-between px-4 container relative"
+          ref={menuRef}
+        >
+          <NavLink to="/" className="font-bold" onClick={closeMenu}>
             GymBuddy
           </NavLink>
           <BiMenuAltRight
@@ -29,16 +50,40 @@ const Navbar = () => {
 
           <div
             className={`bg-white flex-col gap-4 absolute duration-300 left-0 top-8  p-4 w-full ${
-              toggle ? "visible opacity-100" : "invisible opacity-0"
+              !toggle ? "visible opacity-100" : "invisible opacity-0"
             } `}
           >
             <ul className="flex flex-col items-center gap-8">
-              <NavLink to="/workout-plans">Workout Plans</NavLink>
-              <NavLink to="/exercise-library">Exercise Library</NavLink>
-              <NavLink to="/about-us">About Us</NavLink>
+              <NavLink
+                to="/workout-plans"
+                onClick={closeMenu}
+                className={({ isActive }) =>
+                  `${isActive && "border-b-2 border-black font-bold"}`
+                }
+              >
+                Workout Plans
+              </NavLink>
+              <NavLink
+                to="/exercise-library"
+                onClick={closeMenu}
+                className={({ isActive }) =>
+                  `${isActive && "border-b-2 border-black font-bold"}`
+                }
+              >
+                Exercise Library
+              </NavLink>
+              <NavLink
+                to="/about-us"
+                onClick={closeMenu}
+                className={({ isActive }) =>
+                  `${isActive && "border-b-2 border-black font-bold"}`
+                }
+              >
+                About Us
+              </NavLink>
             </ul>
 
-            <div className="flex items-center mt-12 flex-col gap-2 ">
+            <div className="flex items-center mt-12 flex-col gap-2">
               {user && (
                 <div className="flex md:flex-row flex-col gap-2 items-center">
                   <span className="text-sm flex items-center gap-2">
@@ -57,12 +102,14 @@ const Navbar = () => {
                 <>
                   <NavLink
                     to="/sign-up"
+                    onClick={closeMenu}
                     className="px-8 text-xs py-2 rounded-sm bg-white border border-black"
                   >
                     Sign Up
                   </NavLink>
                   <NavLink
                     to="/login"
+                    onClick={closeMenu}
                     className="px-8 text-xs py-2 rounded-sm bg-black text-white border border-white"
                   >
                     Login
@@ -73,7 +120,7 @@ const Navbar = () => {
           </div>
         </nav>
       ) : (
-        <nav className=" items-center gap-4 container flex ">
+        <nav className=" items-center gap-4 container flex">
           <NavLink to="/" className="font-bold flex-1 text-3xl">
             GYMFIT
           </NavLink>
